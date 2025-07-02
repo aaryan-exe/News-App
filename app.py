9a1094ef2e90b9368ff85f082b22a60f17766cda
Aryan Patil <125805392+aaryan-exe@users.noreply.github.com>
2025-07-02 19:45:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import requests
 app = ctk.CTk()
 app.title("News App")
 app.geometry("1000x600")
-app._set_appearance_mode("dark")
+app._set_appearance_mode("light")
+app.configure(fg_color="#D9D9D9")
 # app.resizable(False, False)
 
-headerframe = ctk.CTkFrame(master=app, fg_color="transparent", height=100)
-headerframe.pack(pady=2, fill="both")
+mainFrame=ctk.CTkScrollableFrame(app,fg_color="transparent")
+mainFrame.pack(fill='both',expand='True', pady=20)
+
+headerframe = ctk.CTkFrame(master=mainFrame, fg_color="transparent", height=100)
+headerframe.pack( fill="both")
 
 # Left frame for Breaking News label
 left_frame = ctk.CTkFrame(master=headerframe, fg_color="transparent")
@@ -16,25 +20,25 @@ left_frame.pack(side="left", padx=20, pady=20)
 
 AppName = ctk.CTkLabel(
     master=left_frame,
-    text="Breaking News",
-    font=ctk.CTkFont(family="SF Pro", size=32, weight="bold"),
+    text="Newsberg",
+    font=ctk.CTkFont(family="Georgia", size=42),
+    text_color="Black"
 )
-AppName.pack()
+AppName.pack(pady=(4,4))
 
 # Right frame for dropdown menu (if any)
 right_frame = ctk.CTkFrame(master=headerframe, fg_color="transparent")
 right_frame.pack(side="right", padx=20, pady=20)
 
 # Place SearchBar and SearchBtn inside center_frame instead of right_frame
-SearchBar = ctk.CTkTextbox(master=right_frame, height=30, width=300)
+SearchBar = ctk.CTkTextbox(master=right_frame, height=30, width=250, fg_color="white", text_color="black")
 SearchBar.pack(side="left", padx=(0, 10), pady=10)
 
-SearchBtn = ctk.CTkButton(master=right_frame, text="Search")
+SearchBtn = ctk.CTkButton(master=right_frame, text="Search", fg_color="black", text_color="white", width=80)
 SearchBtn.pack(side="right", pady=10)
 
 
-mainFrame=ctk.CTkScrollableFrame(app,fg_color="transparent")
-mainFrame.pack(fill='both',expand='True', pady=20)
+
 # Trial news
 api_key = "Your_Api_Key"  
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
@@ -45,24 +49,38 @@ data = response.json()
 
 for i in range(10):
     title = data["articles"][i]["title"]
-    author=data["articles"][i]["author"]
-    News1Frame=ctk.CTkFrame(master=mainFrame,)
-    News1Frame.pack(anchor="w",fill="x", padx=10, pady=10)
+    author = data["articles"][i]["author"]
+    description = data["articles"][i]["description"]
+
+    News1Frame = ctk.CTkFrame(master=mainFrame, fg_color="white")
+    News1Frame.pack(anchor="w", fill="x", padx=10, pady=10)
 
     TitleLbl = ctk.CTkLabel(
         News1Frame,
         text=title,
-        font=ctk.CTkFont(family="SF Pro", size=20),
+        font=ctk.CTkFont(family="SF Pro", size=20, weight="bold"),
         justify="left",
-        # wraplength=900,
+        text_color="black"
     )
-    TitleLbl.pack(anchor="w", padx=20, pady=(10,2))
+    TitleLbl.pack(anchor="w", padx=20, pady=(12, 4))
 
-    AuthorLbl=ctk.CTkLabel(master=News1Frame,
-                      text=f"author: {author}",
-                      font=ctk.CTkFont(family="SF Pro", size=12),
-                      text_color="blue",
-                      justify="left"
-                      )
-    AuthorLbl.pack(anchor="w", padx=20, pady=(4,10))
+    AuthorLbl = ctk.CTkLabel(
+        master=News1Frame,
+        text=f"Author: {author}",
+        font=ctk.CTkFont(family="SF Pro", size=12),
+        text_color="blue",
+        justify="left"
+    )
+    AuthorLbl.pack(anchor="w", padx=20, pady=(0, 4))
+
+    DescriptionLbl = ctk.CTkLabel(
+        master=News1Frame,
+        text=description,
+        font=ctk.CTkFont(family="SF Pro", size=16),
+        text_color="black",
+        justify="left",
+        wraplength=900
+    )
+    DescriptionLbl.pack(anchor="w", padx=20, pady=(0, 10))
+
 app.mainloop()
